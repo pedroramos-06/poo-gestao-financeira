d21f7552deed588baca000a02ec0810ec4e1a35d
Pedro Inácio <pedroramos120606@gmail.com>
2026-06-16 14:13:37 -0300

    Initial commit: Estrutura base do Spring Boot

---
+++ src/main/java/WalletFlow/sistemagestaofinanceira/SistemagestaofinanceiraApplication.java
@@ -0,0 +1,13 @@
+package WalletFlow.sistemagestaofinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemagestaofinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemagestaofinanceiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WalletFlow/sistemagestaofinanceira/SistemagestaofinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package WalletFlow.sistemagestaofinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemagestaofinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
