ddc856f08f3be4f2b4e9a95405cab6b113d0ca17
Ana Clara Amaral <anaclara.bany@gmail.com>
2026-06-29 02:47:38 -0300

    feat: adiciona validações de transações

--- src/main/java/WalletFlow/sistemagestaofinanceira/service/TransacaoService.java
+++ src/main/java/WalletFlow/sistemagestaofinanceira/service/TransacaoService.java
@@ -2,10 +2,14 @@ package WalletFlow.sistemagestaofinanceira.service;
 
 import WalletFlow.sistemagestaofinanceira.dto.FiltrosTransacaoDTO;
 import WalletFlow.sistemagestaofinanceira.dto.NovaTransacaoDTO;
+import WalletFlow.sistemagestaofinanceira.enums.TipoTransacao;
+import WalletFlow.sistemagestaofinanceira.exceptions.AcessoNegadoException;
+import WalletFlow.sistemagestaofinanceira.exceptions.SaldoInsuficienteException;
 import WalletFlow.sistemagestaofinanceira.models.Transacao;
 import WalletFlow.sistemagestaofinanceira.models.Usuario;
 import WalletFlow.sistemagestaofinanceira.repository.TransacaoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,14 +20,15 @@ public class TransacaoService {
     public TransacaoService(TransacaoRepository transacaoRepository) {
         this.transacaoRepository = transacaoRepository;
     }
-
-    public void salvar(NovaTransacaoDTO dto, Usuario usuario) {
+    @Transactional
+    public void salvar(NovaTransacaoDTO dto, Usuario usuario) throws SaldoInsuficienteException {
         Transacao transacao = dto.toEntity();
         transacao.setUsuario(usuario);
-
+        validarTransacao(transacao);
         transacaoRepository.save(transacao);
     }
 
+    @Transactional(readOnly = true)
     public List<Transacao> listar(Usuario usuario, FiltrosTransacaoDTO filtros) {
         return transacaoRepository.listar(
             usuario.getId(),
@@ -34,19 +39,56 @@ public class TransacaoService {
         );
     }
 
-    public Transacao buscarPorId(Long id) {
-        return transacaoRepository.findById(id).orElseThrow(() -> new RuntimeException("Transação não encontrada"));
+    @Transactional(readOnly = true)
+    public Transacao buscarPorId(Long id, Long usuarioId) throws AcessoNegadoException {
+        Transacao transacao = transacaoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Transação não encontrada"));
+
+        if (!transacao.getUsuario().getId().equals(usuarioId)) {
+            throw new AcessoNegadoException();
+        }
+
+        return transacao;
     }
 
-    public void excluir(Long id) {
+    @Transactional
+    public void excluir(Long id, Long usuarioId) throws AcessoNegadoException{
+        Transacao transacao = buscarPorId(id, usuarioId);
+
         transacaoRepository.deleteById(id);
     }
 
-    public void editar(NovaTransacaoDTO dto, Usuario usuario) {
+    @Transactional
+    public void editar(NovaTransacaoDTO dto, Usuario usuario) throws AcessoNegadoException, SaldoInsuficienteException {
+        Transacao antiga = buscarPorId(dto.getId(), usuario.getId());
         Transacao transacao = dto.toEntity();
         transacao.setUsuario(usuario);
         transacao.setId(dto.getId());
+        validarTransacao(transacao, antiga.getValor(), antiga.getTipo());
 
         transacaoRepository.save(transacao);
+    }
+
+    private void validarTransacao(Transacao t) throws SaldoInsuficienteException{
+        validarTransacao(t, 0.0, null);
+    }
+
+    private void validarTransacao(Transacao t, double valorAntigo, TipoTransacao tipoAntigo) throws SaldoInsuficienteException {
+        if (t.getTipo() == TipoTransacao.SAIDA) {
+            double saldoAtual = transacaoRepository.getSaldo(t.getUsuario().getId());
+            double saldoDisponivel = saldoAtual;
+
+            if (t.getId() != null) {
+                if (tipoAntigo == TipoTransacao.SAIDA) {
+                    saldoDisponivel += valorAntigo;
+                } else if (tipoAntigo == TipoTransacao.ENTRADA) {
+                    saldoDisponivel -= valorAntigo;
+                }
+            }
+
+            if (t.getValor() > saldoDisponivel) {
+                throw new SaldoInsuficienteException();
+            }
+        }
     }
 }
