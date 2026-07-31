e5bd735dc05238b632dae08acfdfe6cddb3e8645
Ana Clara Amaral <anaclara.bany@gmail.com>
2026-07-31 16:35:21 -0300

    fix: filtro de listagem de transações

--- src/main/java/WalletFlow/sistemagestaofinanceira/exceptions/CategoriaProtegidaException.java
+++ src/main/java/WalletFlow/sistemagestaofinanceira/exceptions/CategoriaProtegidaException.java
@@ -2,6 +2,6 @@ package WalletFlow.sistemagestaofinanceira.exceptions;
 
 public class CategoriaProtegidaException extends RuntimeException {
     public CategoriaProtegidaException() {
-        super("Essa categoria não pode ser deletada");
+        super("Essa categoria não pode ser alterada");
     }
 }
